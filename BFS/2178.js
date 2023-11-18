35c8326e5c2a3f3109daeb1d7bc3d4814176393a
Seungwon Lee <seungwon.dev@gmail.com>
2023-11-18 11:27:48 +0900

    refactor 2178: 미로 탐색
    - 입출력에 필요하지 않은 trim() 제거
    - 불필요한 익명함수 제거
    - let -> const 변경
    - for -> forEach 변경

--- BFS/2178.js
+++ BFS/2178.js
@@ -1,30 +1,33 @@
 const input = require('fs')
   .readFileSync(process.platform == 'linux' ? 'dev/stdin' : __dirname + '/../input.txt')
   .toString()
-  .trim()
   .split('\n')
 
-const [N, M] = input[0].split(' ').map((e) => Number(e))
+const [N, M] = input[0].split(' ').map(Number)
 const maze = input.slice(1, N + 1).map((e) => e.split('').map(Number))
-const dy = [1, 0, -1, 0]
-const dx = [0, 1, 0, -1]
-
-let dist = Array.from(new Array(N), () => new Array(M).fill(-1))
-let queue = [[0, 0]]
-dist[0][0] = 1
+const direction = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+]
+const distance = Array.from(new Array(N), () => new Array(M).fill(-1))
+const queue = [[0, 0]]
+distance[0][0] = 1
 
 while (queue.length != 0) {
   const [y, x] = queue.shift()
-  for (let i = 0; i < 4; i++) {
-    const [ny, nx] = [y + dy[i], x + dx[i]]
+
+  direction.forEach((dir) => {
+    const [ny, nx] = [y + dir[0], x + dir[1]]
 
     if (-1 < ny && ny < N && -1 < nx && nx < M) {
-      if (maze[ny][nx] == 1 && dist[ny][nx] == -1) {
-        dist[ny][nx] = dist[y][x] + 1
+      if (maze[ny][nx] == 1 && distance[ny][nx] == -1) {
+        distance[ny][nx] = distance[y][x] + 1
         queue.push([ny, nx])
       }
     }
-  }
+  })
 }
 
-console.log(dist[N - 1][M - 1])
+console.log(distance[N - 1][M - 1])
